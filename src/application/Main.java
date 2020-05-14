7675b8094e3ac7701666c586be4affa237513620
mariusraht1 <mariusraht+1@gmail.com>
2020-05-14 18:29:21 +0200

    Init

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,29 +14,29 @@ import javafx.scene.Scene;
  * @version 14.05.2020-001
  */
 public class Main extends Application {
-	private static Stage primaryStage;
+    private static Stage primaryStage;
 
-	public static Stage getPrimaryStage() {
-		return primaryStage;
+    public static Stage getPrimaryStage() {
+	return primaryStage;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+	try {
+	    Main.primaryStage = primaryStage;
+
+	    primaryStage.setTitle("Fuzzy System");
+	    primaryStage.centerOnScreen();
+
+	    Scene scene = new Scene(FXMLLoader.load(Main.class.getResource("/application/view/MainScene.fxml")));
+	    primaryStage.setScene(scene);
+	    primaryStage.show();
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Main.primaryStage = primaryStage;
-
-			primaryStage.setTitle("Fuzzy System");
-			primaryStage.centerOnScreen();
-
-			Scene scene = new Scene(FXMLLoader.load(Main.class.getResource("/application/view/MainScene.fxml")));
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+	launch(args);
+    }
 }
