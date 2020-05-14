fb514bfc88df0bf6714304446c1a127704a75d45
mariusraht1 <mariusraht+1@gmail.com>
2020-05-14 14:23:49 +0200

    Initialization

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,41 @@
 package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
-
+/**
+ * Fuzzy System for Warehouse Ordering System
+ * 
+ * @author Marius Raht
+ * @version 14.05.2020-001
+ */
 public class Main extends Application {
+	private static Stage primaryStage;
+
+	public static Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Main.primaryStage = primaryStage;
+
+			primaryStage.setTitle("Fuzzy System");
+			primaryStage.centerOnScreen();
+
+			Scene scene = new Scene(FXMLLoader.load(Main.class.getResource("/application/view/MainScene.fxml")));
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
